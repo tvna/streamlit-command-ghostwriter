78cd55751b3deb62aaefff3fd493083f7d4ee16d
tvna <tsubasa.nagano@icloud.com>
2024-05-21 06:21:11 +0900

    Replaced pytest contents of command_render with unit test by parametrize.

--- tests/test_render.py
+++ tests/test_render.py
@@ -1,64 +1,51 @@
 from io import BytesIO
+from typing import Any, Dict, Optional
+
+import pytest
 
 from features.command_render import GhostwriterRender
 
 
-def test_render_template_success() -> None:
-    template_content = b"Hello {{ name }}!"
-    template_file = BytesIO(template_content)
-    context = {"name": "World"}
-    render = GhostwriterRender()
-    render.load_template_file(template_file).apply_context(context)
-    result = render.render_content
-    assert result == "Hello World!"
+@pytest.mark.parametrize(
+    (
+        "is_strict_undefined",
+        "is_remove_multiple_newline",
+        "template_content",
+        "context",
+        "is_successful",
+        "result_message",
+        "error_message",
+    ),
+    [
+        # Test case on success
+        pytest.param(True, True, b"Hello {{ name }}!", {"name": "World"}, True, "Hello World!", None),
+        pytest.param(False, True, b"Hello {{ name }}!", {}, True, "Hello !", None),
+        # Test case on failed
+        pytest.param(True, True, b"Hello {{ user }}!", {"name": "World"}, False, None, "'user' is undefined"),
+        pytest.param(
+            True,
+            True,
+            b"\x80\x81\x82\x83",
+            {"name": "World"},
+            False,
+            None,
+            "'utf-8' codec can't decode byte 0x80 in position 0: invalid start byte",
+        ),
+        pytest.param(True, True, b"Hello {{ name }!", {"name": "World"}, False, None, "unexpected '}'"),
+    ],
+)
+def test_apply(
+    is_strict_undefined: bool,
+    is_remove_multiple_newline: bool,
+    template_content: bytes,
+    context: Dict[str, Any],
+    is_successful: bool,
+    result_message: str,
+    error_message: Optional[str],
+) -> None:
+    render = GhostwriterRender(is_strict_undefined, is_remove_multiple_newline)
+    render.load_template_file(BytesIO(template_content))
 
-
-def test_render_template_not_strict_undefined_success() -> None:
-    template_content = b"Hello {{ name }}!"
-    template_file = BytesIO(template_content)
-    render = GhostwriterRender(False)
-    render.load_template_file(template_file).apply_context({})
-    result = render.render_content
-    assert result == "Hello !"
-
-
-def test_render_template_with_undefined_error() -> None:
-    template_content = b"Hello {{ user }}!"
-    template_file = BytesIO(template_content)
-    context = {"name": "World"}  # 'user' is not defined in the context
-    render = GhostwriterRender()
-    render.load_template_file(template_file).apply_context(context)
-    result = render.render_content
-    assert result is None
-    assert "undefined" in str(render.error_message).lower()
-
-
-def test_render_template_with_unicode_decode_error() -> None:
-    template_content = b"\x80\x81\x82\x83"
-    template_file = BytesIO(template_content)
-    context = {"name": "World"}
-    render = GhostwriterRender()
-    render.load_template_file(template_file).apply_context(context)
-    result = render.render_content
-    assert result is None
-    assert "'utf-8' codec can't decode byte 0x80 in position 0" in str(render.error_message)
-
-
-def test_render_template_with_not_binary_error() -> None:
-    template_file = "Hello {{ user }}!"
-    context = {"name": "World"}
-    render = GhostwriterRender()
-    render.load_template_file(template_file).apply_context(context)  # type: ignore
-    result = render.render_content
-    assert result is None
-    assert "'str' object has no attribute 'read'" in str(render.error_message)
-
-
-def test_render_template_with_syntax_error() -> None:
-    template_content = b"Hello {{ name }!"
-    template_file = BytesIO(template_content)
-    render = GhostwriterRender(False)
-    render.load_template_file(template_file).apply_context({})
-    result = render.render_content
-    assert result is None
-    assert "unexpected '}'" in str(render.error_message)
+    assert render.apply_context(context) == is_successful
+    assert render.render_content == result_message
+    assert render.error_message == error_message
