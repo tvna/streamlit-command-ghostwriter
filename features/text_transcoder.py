1d2edafd543f4ffe66e4d000471232bca0cc585c
tvna <tsubasa.nagano@icloud.com>
2024-06-29 22:28:52 +0900

    feat: test_transcoderにバイナリ判定を追加
    - バイナリ判定を行うdetect_binaryを追加
    - detect_encodingの対象ファイルを引数から取得するように変更
    - バイナリ判定の追加に伴うテストケースの追加

--- features/text_transcoder.py
+++ features/text_transcoder.py
@@ -9,13 +9,26 @@ class TextTranscoder:
         self.__input_data: Final[BytesIO] = input_data
         self.__filename: Final[str] = input_data.name
 
-    def detect_encoding(self: "TextTranscoder") -> Optional[str]:
-        raw_data = self.__input_data.getvalue()
+    def detect_binary(self: "TextTranscoder", input_data: BytesIO) -> bool:
+        current_position = input_data.tell()
+        input_data.seek(0)
+        chunk = input_data.read(1024)
+        input_data.seek(current_position)
+
+        return b"\0" in chunk
+
+    def detect_encoding(self: "TextTranscoder", input_data: BytesIO) -> Optional[str]:
+        if self.detect_binary(input_data):
+            return None
+
+        input_data.seek(0)
+        raw_data = input_data.getvalue()
         result = nkf.guess(raw_data)  # type: ignore
+
         return result
 
     def to_utf8(self: "TextTranscoder") -> Optional[BytesIO]:
-        encoding = self.detect_encoding()
+        encoding = self.detect_encoding(self.__input_data)
 
         if not encoding:
             return None
--- tests/test_transcoder.py
+++ tests/test_transcoder.py
@@ -12,23 +12,37 @@ from features.text_transcoder import TextTranscoder
     [
         pytest.param("ABCDEF", "Shift_JIS", "ASCII", "ABCDEF"),
         pytest.param("ABCDEF", "Shift-JIS", "ASCII", "ABCDEF"),
+        pytest.param("ABCDEF", "EUC-JP", "ASCII", "ABCDEF"),
+        pytest.param("ABCDEF", "EUC_JP", "ASCII", "ABCDEF"),
+        pytest.param("ABCDEF", "utf-8", "ASCII", "ABCDEF"),
+        pytest.param("ABCDEF", "utf_8", "ASCII", "ABCDEF"),
+        pytest.param("", "Shift_JIS", "ASCII", ""),
+        pytest.param("", "Shift-JIS", "ASCII", ""),
+        pytest.param("", "EUC_JP", "ASCII", ""),
+        pytest.param("", "EUC-JP", "ASCII", ""),
+        pytest.param("", "utf-8", "ASCII", ""),
+        pytest.param("", "utf_8", "ASCII", ""),
         pytest.param("あいうえお", "Shift_JIS", "Shift_JIS", "あいうえお"),
         pytest.param("あいうえお", "Shift-JIS", "Shift_JIS", "あいうえお"),
-        pytest.param("漢字による試験", "Shift_JIS", "Shift_JIS", "漢字による試験"),
-        pytest.param("漢字による試験", "Shift-JIS", "Shift_JIS", "漢字による試験"),
         pytest.param("あいうえお", "EUC-JP", "EUC-JP", "あいうえお"),
         pytest.param("あいうえお", "EUC_JP", "EUC-JP", "あいうえお"),
+        pytest.param("あいうえお", "utf-8", "UTF-8", "あいうえお"),
+        pytest.param("あいうえお", "utf_8", "UTF-8", "あいうえお"),
+        pytest.param("漢字による試験", "Shift_JIS", "Shift_JIS", "漢字による試験"),
+        pytest.param("漢字による試験", "Shift-JIS", "Shift_JIS", "漢字による試験"),
         pytest.param("漢字による試験", "EUC-JP", "EUC-JP", "漢字による試験"),
         pytest.param("漢字による試験", "EUC_JP", "EUC-JP", "漢字による試験"),
+        pytest.param("漢字による試験", "utf-8", "UTF-8", "漢字による試験"),
+        pytest.param("漢字による試験", "utf_8", "UTF-8", "漢字による試験"),
     ],
 )
-def test_transcoder(input_str: str, input_encoding: str, expected_encoding: Optional[str], expected_result: Optional[BytesIO]) -> None:
+def test_transcoder(input_str: str, input_encoding: str, expected_encoding: str, expected_result: str) -> None:
     input_data = BytesIO(input_str.encode(input_encoding))
     input_data.name = "example.csv"
 
     trans = TextTranscoder(input_data)
-    print(trans.detect_encoding())
-    assert trans.detect_encoding() == expected_encoding
+    print(trans.detect_encoding(input_data))
+    assert trans.detect_encoding(input_data) == expected_encoding
 
     output_data = trans.to_utf8()
     assert output_data.read().decode("utf-8") == expected_result  # type: ignore
@@ -40,8 +54,9 @@ def test_transcoder(input_str: str, input_encoding: str, expected_encoding: Opti
 @pytest.mark.parametrize(
     ("input_bytes", "expected_encoding", "expected_result"),
     [
-        pytest.param(b"\x00\x01\x02\x03\x04", "ASCII", b"\x00\x01\x02\x03\x04"),
+        pytest.param(b"\x00\x01\x02\x03\x04", None, b"\x00\x01\x02\x03\x04"),
         pytest.param(b"\x80\x81\x82\x83", "EUC-JP", b"\x80\x81\x82\x83"),
+        pytest.param(b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01", None, b"\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01"),
     ],
 )
 def test_transcoder_non_string(input_bytes: bytes, expected_encoding: Optional[str], expected_result: Optional[bytes]) -> None:
@@ -49,12 +64,11 @@ def test_transcoder_non_string(input_bytes: bytes, expected_encoding: Optional[s
     input_data.name = "example.csv"
 
     trans = TextTranscoder(input_data)
-    print(trans.detect_encoding())
-    assert trans.detect_encoding() == expected_encoding
+    assert trans.detect_encoding(input_data) == expected_encoding
 
     output_data = trans.to_utf8()
     if isinstance(output_data, BytesIO):
-        assert output_data.read() == expected_result
+        assert output_data.getvalue() == expected_result
         assert trans.challenge_to_utf8().getvalue() == expected_result
         assert output_data.name == input_data.name
     else:
