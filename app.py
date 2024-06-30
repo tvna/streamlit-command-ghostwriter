9c79819d871b5f841471bd453f3d6cd49b422e01
tvna <tsubasa.nagano@icloud.com>
2024-06-30 15:15:38 +0900

    refactor: app.pyのリファクタリング
    - フォーム経由のsession_stateの更新を、全てkeyに統一
    - textsによる辞書データの受け渡しをBoxによるネスト付きドットアクセスに変更

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from typing import Dict, Final, Optional
+from typing import Final, Optional
 
 import streamlit as st
+from box import Box
 
 from features.core import GhostwriterCore
 from i18n import LANGUAGES
 
 
 class TabViewModel:
-    def __init__(self: "TabViewModel", texts: Dict[str, str]) -> None:
-        self.__texts: Final[Dict[str, str]] = texts
+    def __init__(self: "TabViewModel", texts: Box) -> None:
+        self.__texts: Final[Box] = texts
         self.__execute_mode: int = 0
 
     def set_execute_mode(self: "TabViewModel", is_text: bool, is_markdown: bool, is_debug: bool) -> "TabViewModel":
@@ -40,16 +41,16 @@ class TabViewModel:
         elif (3 > self.__execute_mode > 0) and isinstance(result_text, str):
             self.__show_tab1_result(result_text)
         elif 3 > self.__execute_mode > 0:
-            st.warning(self.__texts["tab1_error_both_files"])
+            st.warning(self.__texts.tab1.error_both_files)
 
     def __show_tab1_result(self: "TabViewModel", result_text: str) -> None:
         """Show tab1 success content."""
 
         if self.__execute_mode == 1:
-            st.success(self.__texts["tab1_success_formatted_text"])
-            st.container(border=True).text_area(self.__texts["tab1_formatted_text"], result_text, key="tab1_result_textarea", height=500)
+            st.success(self.__texts.tab1.success_formatted_text)
+            st.container(border=True).text_area(self.__texts.tab1.formatted_text, result_text, key="tab1_result_textarea", height=500)
         elif self.__execute_mode == 2:
-            st.success(self.__texts["tab1_success_formatted_text"])
+            st.success(self.__texts.tab1.success_formatted_text)
             st.container(border=True).markdown(result_text)
 
     def __show_tab1_error(self: "TabViewModel", first_error_message: Optional[str], second_error_message: Optional[str]) -> None:
@@ -68,21 +69,23 @@ class TabViewModel:
         if self.__execute_mode < 3:
             return
         elif not filename or not parsed_config:
-            st.warning(f"{self.__texts['tab2_error_debug_not_found']}")
+            st.warning(f"{self.__texts.tab2.error_debug_not_found}")
             return
 
-        st.success(self.__texts["tab2_success_debug_config"])
-        st.text_area(self.__texts["tab2_debug_config_text"], parsed_config, key="tab2_result_textarea", height=500)
+        st.success(self.__texts.tab2.success_debug_config)
+        st.text_area(self.__texts.tab2.debug_config_text, parsed_config, key="tab2_result_textarea", height=500)
 
 
 def main() -> None:
     """Generate Streamlit web screens."""
-    texts: Final[Dict[str, str]] = LANGUAGES["日本語"]
 
     app_title: Final[str] = "Command ghostwriter"
     app_icon: Final[str] = ":ghost:"
     default_format_type: Final[int] = 3
     config_file_exts: Final[list] = ["toml", "yaml", "yml", "csv"]
+    default_language = "日本語"
+
+    texts: Final[Box] = Box(LANGUAGES[default_language])
 
     st.session_state.update(
         {
@@ -95,31 +98,29 @@ def main() -> None:
     st.title(f"{app_title} {app_icon}")
 
     with st.sidebar:
-        st.write(texts["sidebar_welcome"])
-
-        with st.expander(texts["sidebar_syntax_of_each_file"], expanded=True):
-            st.markdown(
-                f"""
-            - [toml syntax docs]({texts["sidebar_toml_syntax_doc"]})
-            - [yaml syntax docs]({texts["sidebar_yaml_syntax_doc"]})
-            - [jinja syntax docs]({texts["sidebar_jinja_syntax_doc"]})
+        st.write(texts.sidebar.welcome)
+        st.expander(texts.sidebar.syntax_of_each_file, expanded=True).markdown(
+            f"""
+            - [toml syntax docs]({texts.sidebar.toml_syntax_doc})
+            - [yaml syntax docs]({texts.sidebar.yaml_syntax_doc})
+            - [jinja syntax docs]({texts.sidebar.jinja_syntax_doc})
             """
-            )
+        )
 
-    tabs = st.tabs([texts["tab1_menu_convert_text"], texts["tab2_menu_debug_config"], texts["tab3_menu_advanced_option"]])
+    tabs = st.tabs([texts.tab1.menu_title, texts.tab2.menu_title, texts.tab3.menu_title])
 
     with tabs[0]:
-        st.subheader(texts["tab1_subheader"], divider="rainbow")
+        st.subheader(texts.tab1.subheader, divider="rainbow")
         tab1_row1 = st.columns(2)
         tab1_row2 = st.columns(3)
 
-        tab1_row1[0].container(border=True).file_uploader(texts["tab1_upload_config"], type=config_file_exts, key="tab1_config_file")
-        tab1_row1[1].container(border=True).file_uploader(texts["tab1_upload_template"], type=["jinja2", "j2"], key="tab1_template_file")
+        tab1_row1[0].container(border=True).file_uploader(texts.tab1.upload_config, type=config_file_exts, key="tab1_config_file")
+        tab1_row1[1].container(border=True).file_uploader(texts.tab1.upload_template, type=["jinja2", "j2"], key="tab1_template_file")
 
-        tab1_row2[0].button(texts["tab1_generate_text_button"], use_container_width=True, key="tab1_execute_text")
-        tab1_row2[1].button(texts["tab1_generate_markdown_button"], use_container_width=True, key="tab1_execute_markdown")
+        tab1_row2[0].button(texts.tab1.generate_text_button, use_container_width=True, key="tab1_execute_text")
+        tab1_row2[1].button(texts.tab1.generate_markdown_button, use_container_width=True, key="tab1_execute_markdown")
 
-        tab1_model = GhostwriterCore(texts["tab1_error_toml_parse"], texts["tab1_error_template_generate"])
+        tab1_model = GhostwriterCore(texts.tab1.error_toml_parse, texts.tab1.error_template_generate)
         tab1_model.load_config_file(
             st.session_state.get("tab1_config_file"),
             st.session_state.get("csv_rows_name", "csv_rows"),
@@ -141,8 +142,8 @@ def main() -> None:
         )
 
         tab1_row2[2].download_button(
-            label=texts["tab1_download_button"],
-            data=st.session_state.get("tab1_result_content", None) or "No data available",
+            label=texts.tab1.download_button,
+            data=st.session_state.get("tab1_result_content") or "No data available",
             file_name=tab1_model.get_download_filename(
                 st.session_state.get("download_filename", "command"),
                 st.session_state.get("download_file_ext", "txt"),
@@ -158,26 +159,26 @@ def main() -> None:
             st.session_state.get("tab1_execute_markdown", False),
             st.session_state.get("tab2_execute", False),
         ).show_tab1(
-            st.session_state.get("tab1_result_content", None),
-            st.session_state.get("tab1_error_config", None),
-            st.session_state.get("tab1_error_template", None),
+            st.session_state.get("tab1_result_content"),
+            st.session_state.get("tab1_error_config"),
+            st.session_state.get("tab1_error_template"),
         )
 
     with tabs[1]:
-        st.subheader(texts["tab2_subheader"], divider="rainbow")
+        st.subheader(texts.tab2.subheader, divider="rainbow")
         tab2_row1 = st.columns(2)
         tab2_row2 = st.columns(3)
 
-        tab2_row1[0].container(border=True).file_uploader(texts["tab2_upload_debug_config"], type=config_file_exts, key="tab2_config_file")
+        tab2_row1[0].container(border=True).file_uploader(texts.tab2.upload_debug_config, type=config_file_exts, key="tab2_config_file")
 
-        tab2_model = GhostwriterCore(texts["tab2_error_debug_config"])
+        tab2_model = GhostwriterCore(texts.tab2.error_debug_config)
         tab2_model.load_config_file(
             st.session_state.get("tab2_config_file"),
             st.session_state.get("csv_rows_name", "csv_rows"),
             st.session_state.get("is_auto_transcoding", True),
         )
 
-        tab2_row2[0].button(texts["tab2_generate_debug_config"], use_container_width=True, key="tab2_execute")
+        tab2_row2[0].button(texts.tab2.generate_debug_config, use_container_width=True, key="tab2_execute")
 
         st.session_state.update(
             {
@@ -192,37 +193,37 @@ def main() -> None:
             st.session_state.get("tab1_execute_markdown", False),
             st.session_state.get("tab2_execute", False),
         ).show_tab2(
-            st.session_state.get("tab2_result_content", ""),
+            st.session_state.get("tab2_result_content"),
             tab2_model.get_uploaded_filename(st.session_state.get("tab2_config_file")),
-            st.session_state.get("tab2_error_config", None),
+            st.session_state.get("tab2_error_config"),
         )
 
     with tabs[2]:
         tab3_row1 = st.columns(2)
 
         with tab3_row1[0].container(border=True):
-            st.subheader(texts["tab3_subheader_input_file"])
-            st.session_state.csv_rows_name = st.container(border=True).text_input(texts["tab3_csv_rows_name"], "csv_rows")
-            st.session_state.is_strict_undefined = st.container(border=True).toggle(texts["tab3_strict_undefined"], value=True)
-            st.session_state.is_auto_transcoding = st.container(border=True).toggle(texts["tab3_auto_encoding"], value=True)
+            st.subheader(texts.tab3.subheader_input_file)
+            st.container(border=True).text_input(texts.tab3.csv_rows_name, value="csv_rows", key="csv_rows_name")
+            st.container(border=True).toggle(texts.tab3.strict_undefined, value=True, key="is_strict_undefined")
+            st.container(border=True).toggle(texts.tab3.auto_encoding, value=True, key="is_auto_transcoding")
+
         with tab3_row1[1].container(border=True):
-            st.subheader(texts["tab3_subheader_output_file"])
-            st.session_state.result_format_type = st.container(border=True).selectbox(
-                texts["tab3_format_type"],
+            st.subheader(texts.tab3.subheader_output_file)
+            st.container(border=True).selectbox(
+                texts.tab3.format_type,
                 (
-                    texts["tab3_format_type_item0"],
-                    texts["tab3_format_type_item1"],
-                    texts["tab3_format_type_item2"],
-                    texts["tab3_format_type_item3"],
-                    texts["tab3_format_type_item4"],
+                    texts.tab3.format_type_item0,
+                    texts.tab3.format_type_item1,
+                    texts.tab3.format_type_item2,
+                    texts.tab3.format_type_item3,
+                    texts.tab3.format_type_item4,
                 ),
                 index=default_format_type,
+                key="result_format_type",
             )
-            st.session_state.download_filename = st.container(border=True).text_input(texts["tab3_download_filename"], "command")
-            st.session_state.is_append_timestamp = st.container(border=True).toggle(texts["tab3_append_timestamp_filename"], value=True)
-            st.session_state.download_file_ext = st.container(border=True).radio(
-                texts["tab3_download_file_extension"], ["txt", "md"], horizontal=True
-            )
+            st.container(border=True).text_input(texts.tab3.download_filename, "command", key="download_filename")
+            st.container(border=True).toggle(texts.tab3.append_timestamp_filename, value=True, key="is_append_timestamp")
+            st.container(border=True).radio(texts.tab3.download_file_extension, ["txt", "md"], horizontal=True, key="download_file_ext")
 
 
 if __name__ == "__main__":
--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -8,6 +8,9 @@ def test_i11n_japanese() -> None:
     assert isinstance(LANGUAGES, dict)
     assert "日本語" in LANGUAGES.keys()
 
-    for k, v in LANGUAGES["日本語"].items():
-        assert isinstance(k, str)
-        assert isinstance(v, str)
+    for nest1_key, nest1_val in LANGUAGES["日本語"].items():
+        assert isinstance(nest1_key, str)
+        assert isinstance(nest1_val, dict)
+        for nest2_key, nest2_val in nest1_val.items():
+            assert isinstance(nest2_key, str)
+            assert isinstance(nest2_val, str)
